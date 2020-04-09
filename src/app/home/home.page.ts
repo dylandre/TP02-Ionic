f3f831e10feba4eb5facabe49181b6285941d578
ANDRE Dylan <dylan.andre@ynov.com>
2020-04-09 17:26:59 +0200

    Ajout du service Natif de la caméra

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-home',
@@ -8,14 +10,48 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   title: string;
+  imgData: string;
 
-  constructor() {}
+  constructor(private alertController: AlertController, private camera: Camera) {}
 
   updateTitle() {
     this.title = 'Mon Nouveau Titre';
   }
 
-  PasTester() {
-    this.title = 'A Tester !';
+  /**
+   * https://ionicframework.com/docs/api/alert
+   */
+  async fireAlert() {
+    // creation de l alerte
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+    // quand l alerte sera masquée
+    alert.onDidDismiss().then(() => console.log('alerte masquée'))
+
+    // affichage de l alerte
+    await alert.present();
+  }
+
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true
+    };
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      console.log(imageData);
+      this.imgData = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+    });
   }
 }
